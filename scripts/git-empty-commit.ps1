dd607f2770cbf44798b2413a5e9c53caa4453962
changchengfeng <changchengfeng001@gmail.com>
2026-09-15 10:53:31 +0800

    chore: prevent Cursor co-author from appearing in commits

--- scripts/git-empty-commit.ps1
+++ scripts/git-empty-commit.ps1
@@ -5,6 +5,6 @@ $env:GIT_AUTHOR_EMAIL = "changchengfeng001@gmail.com"
 $env:GIT_COMMITTER_NAME = "changchengfeng"
 $env:GIT_COMMITTER_EMAIL = "changchengfeng001@gmail.com"
 $tree = git rev-parse "HEAD^{tree}"
-$new = git commit-tree $tree -p HEAD -m $Message
+$new = & git.exe commit-tree $tree -p HEAD -m $Message
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 git reset --hard $new
